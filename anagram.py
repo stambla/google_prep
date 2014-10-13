9a713ae4bec6ba039d6947cbf29f1c0d0f7ae59b
stambla <stamblewicz@yahoo.ie>
2014-10-13 16:03:36 +0100

    add speed test

--- anagram.py
+++ anagram.py
@@ -5,10 +5,22 @@
     
     written by stambla
 """
+import time
+
+def speed_test(func):
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        #for x in xrange(5000):
+        results = func(*args, **kwargs)
+        t2 = time.time()
+        print '%s took %0.3f ms' % (func.func_name, (t2-t1)*1000.0)
+        return results
+    return wrapper
 
 str_1 = input("Please input first word:")
 str_2 = input("Please input second word:")
 
+@speed_test
 def check_anagram(str_1, str_2):
     temp =  str_1[::-1]
     if temp == str_2:
